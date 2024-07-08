b389c2af62c5fdf099d017b93c005e1f4a38706a
Heradwipradita09 <114724251+Heradwipradita09@users.noreply.github.com>
2024-07-08 17:59:31 +0700

    Update test_apply_leave.js

--- Automation_Testing/test/leave/test_apply_leave.js
+++ Automation_Testing/test/leave/test_apply_leave.js
@@ -5,21 +5,21 @@ describe('Apply Leave', function () {
     let driver;
 
     before(async function () {
-        this.timeout(90000); // Menambahkan timeout yang lebih panjang jika diperlukan
+        this.timeout(90000); // Menambahkan timeout yang lebih panjang 
         driver = await new Builder().forBrowser('chrome').build();
         await driver.get('https://opensource-demo.orangehrmlive.com/');
     });
 
     it('should apply for leave', async function () {
-        this.timeout(90000); // Menambahkan timeout yang lebih panjang jika diperlukan
+        this.timeout(90000); // Menambahkan timeout yang lebih panjang 
         try {
-            await driver.wait(until.elementLocated(By.id('txtUsername')), 90000); // Tunggu hingga elemen txtUsername muncul
+            await driver.wait(until.elementLocated(By.id('txtUsername')), 90000); 
             await driver.findElement(By.id('txtUsername')).sendKeys('Admin');
             await driver.findElement(By.id('txtPassword')).sendKeys('admin123');
             await driver.findElement(By.id('btnLogin')).click();
             await driver.findElement(By.id('menu_leave_viewLeaveModule')).click();
             await driver.findElement(By.id('menu_leave_applyLeave')).click();
-            await driver.wait(until.elementLocated(By.id('applyleave_txtLeaveType')), 10000); // Tunggu hingga elemen applyleave_txtLeaveType muncul
+            await driver.wait(until.elementLocated(By.id('applyleave_txtLeaveType')), 10000); 
             await driver.findElement(By.id('applyleave_txtLeaveType')).sendKeys('Paid Leave', Key.RETURN);
             await driver.findElement(By.id('applyleave_txtFromDate')).sendKeys('2024-07-15');
             await driver.findElement(By.id('applyleave_txtToDate')).sendKeys('2024-07-16');
@@ -33,7 +33,7 @@ describe('Apply Leave', function () {
     });
 
     after(async function () {
-        this.timeout(90000); // Menambahkan timeout yang lebih panjang jika diperlukan
+        this.timeout(90000); // Menambahkan timeout yang lebih panjang 
         await driver.quit();
     });
 });
